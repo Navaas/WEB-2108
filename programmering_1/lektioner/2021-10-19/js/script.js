23bca361685b8534c8279e2608c9ca8412a9e99d
micand89 <89338483+micand89@users.noreply.github.com>
2021-10-19 12:59:26 +0200

    update

--- programmering_1/lektioner/2021-10-19/js/script.js
+++ programmering_1/lektioner/2021-10-19/js/script.js
@@ -11,3 +11,22 @@ function somethingMoreToLoadOnPageRender(){
     document.getElementById('car').innerText = 'Volvo'
 }
 somethingMoreToLoadOnPageRender()
+
+function okButton(){
+    let input = document.getElementById('getInputFromInputString()').value
+    console.log(input)
+    document.getElementById('inputResult').innerText = input
+}
+
+function randomBotton(){
+    document.getElementById('getInputFromInputString()').value = 'Random'
+}
+
+
+let array = ['Alpha', 'Ursula', 'Aram', 'Christoffer', 'Susanna', 'Samatha', 'Hampus', 'Carin', 'Kimmie', Emil]
+
+function pickName(){
+    let choseOne = array[Math.floor( Math.random()*array.length)];
+    console.log(choseOne)
+}
+pickName()
