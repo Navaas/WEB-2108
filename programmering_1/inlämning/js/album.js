54f7048d2db7e495ac0f669e4aba7d86eb2a5c0c
micand89 <89338483+micand89@users.noreply.github.com>
2021-10-23 14:41:17 +0200

    lagt in img och uppgifter i album

--- programmering_1/inlämning/js/album.js
+++ programmering_1/inlämning/js/album.js
@@ -1,53 +1,54 @@
 function byeAlbumsContaioner() {
-    return`
-    <section class="album-one">
-        <img src="img/fivefingerdeathpunch.JPG" alt="sabaton" class="img" id="sabaton">
-            <p>Album: FiveFingerDeathPunch-F8</p>
-            <p>Pris179</p>
+    return `
+    <section>
+        <img src="img/fivefingerdeathpunch.JPG" alt="five" class="img" id="five">
+            <p>Five Finger Death Punch - F8</p>
+            <p>Pris: 179:-</p>
             <button onClick="">Köp</button>
     </section>
-        <section class="album-one">
-        <img src="img/fivefingerdeathpunch.JPG" alt="sabaton" class="img" id="sabaton">
-            <p>Album: FiveFingerDeathPunch-F8</p>
-            <p>Pris179</p>
+        <section>
+        <img src="img/fivefingerdeathpunch2.JPG" alt="five1" class="img" id="five1">
+            <p>Five Finger Death Punch - GotYour6</p>
+            <p>Pris: 179:-</p>
             <button onClick="">Köp</button>
     </section>
-        <section class="album-one">
-        <img src="img/fivefingerdeathpunch.JPG" alt="sabaton" class="img" id="sabaton">
-            <p>Album: FiveFingerDeathPunch-F8</p>
-            <p>Pris179</p>
+        <section>
+        <img src="img/hammerfall.JPG" alt="hammerfall" class="img" id="hammerfall">
+            <p>Hammerfall - Masterpieces</p>
+            <p>Pris: 169:-</p>
             <button onClick="">Köp</button>
     </section>
-        <section class="album-one">
-        <img src="img/fivefingerdeathpunch.JPG" alt="sabaton" class="img" id="sabaton">
-            <p>Album: FiveFingerDeathPunch-F8</p>
-            <p>Pris179</p>
+        <section>
+        <img src="img/hardcoresuperstar.JPG" alt="hardcoresuperstar" class="img" id="hardcoresuperstar">
+            <p>HR - Hardcore Superstar</p>
+            <p>Pris: 149:-</p>
             <button onClick="">Köp</button>
     </section>
-        <section class="album-one">
-        <img src="img/fivefingerdeathpunch.JPG" alt="sabaton" class="img" id="sabaton">
-            <p>Album: FiveFingerDeathPunch-F8</p>
-            <p>Pris179</p>
+        <section>
+        <img src="img/nickelback.JPG" alt="nickelback" class="img" id="nickelback">
+            <p>Best Of Nickelback - Vol.1</p>
+            <p>Pris: 169:-</p>
             <button onClick="">Köp</button>
     </section>
-        <section class="album-one">
-        <img src="img/fivefingerdeathpunch.JPG" alt="sabaton" class="img" id="sabaton">
-            <p>Album: FiveFingerDeathPunch-F8</p>
-            <p>Pris179</p>
+        <section>
+        <img src="img/sabaton.JPG" alt="sabaton" class="img" id="sabaton">
+            <p>Sabaton - Carolus Rex </p>
+            <p>Pris: 159:-</p>
             <button onClick="">Köp</button>
     </section>
-        <section class="album-one">
-        <img src="img/fivefingerdeathpunch.JPG" alt="sabaton" class="img" id="sabaton">
-            <p>Album: FiveFingerDeathPunch-F8</p>
-            <p>Pris179</p>
+        <section>
+        <img src="img/sabaton2.JPG" alt="sabaton2" class="img" id="sabaton2">
+            <p>Sabaton - The Last Stand</p>
+            <p>Pris: 199:-</p>
             <button onClick="">Köp</button>
     </section>
-        <section class="album-one">
-        <img src="img/fivefingerdeathpunch.JPG" alt="sabaton" class="img" id="sabaton">
-            <p>Album: FiveFingerDeathPunch-F8</p>
-            <p>Pris179</p>
+        <section>
+        <img src="img/theoffspring.JPG" alt="theoffspring" class="img" id="theoffspring">
+            <p>The Offspring - Americana</p>
+            <p>Pris: 139:-</p>
             <button onClick="">Köp</button>
     </section>
 `
 }
+
 document.getElementById('allAlbums').innerHTML = byeAlbumsContaioner()
