c5c1fec8c4f2fae161b1d8c41aaed4590021cb60
micand89 <89338483+micand89@users.noreply.github.com>
2022-01-24 19:46:55 +0100

    API

--- webbutveckling_2/2022-01-20/robo-friend/src/App.js
+++ webbutveckling_2/2022-01-20/robo-friend/src/App.js
@@ -1,22 +1,39 @@
-import robots from "./utils/data/Robots";
+/*import robots from "./utils/data/Robots";*/
 import CardList from "./components/CardList";
 import Scroll from "./components/Scroll";
 import SearchBox from "./components/SearchBox";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-    const [searchField, setSearchField] = useState()
+    const [searchField, setSearchField] = useState('')
+    const [robots, setRobots] = useState([])
 
-    return (
-        <div className='tc'>
-            <h1 className='robo-logo f1'>Robofriends</h1>
-            { searchField }
-            <SearchBox setSearchField={setSearchField}/>
-            <Scroll>
-                <CardList robots={robots}/>
-            </Scroll>
-        </div>
-    );
+    const filteredRobot = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+
+    const fetchRobots = () => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {
+                setRobots(users)
+            })
+    }
+
+    useEffect(() => {
+        fetchRobots()
+    }, [])
+
+return (
+    <div className='tc'>
+        <h1 className='robo-logo f1'>Robofriends</h1>
+        {searchField}
+        <SearchBox setSearchField={setSearchField}/>
+        <Scroll>
+            <CardList robots={ filteredRobot }/>
+        </Scroll>
+    </div>
+);
 }
 
 export default App;
