c0462227b4f53324fad5bac20c82d76f6ba502da
micand89 <89338483+micand89@users.noreply.github.com>
2022-03-01 19:34:59 +0100

    Kopplat Backend till Frontend

--- Webbserverprogrammering/lektioner/2022-02-24/backend/src/routes/UserRoutes.js
+++ Webbserverprogrammering/lektioner/2022-02-24/backend/src/routes/UserRoutes.js
@@ -1,4 +1,4 @@
-import UserController from '../controller/UserController'
+import UserController from '../controller/UserController.js'
 
 // Endpoint + Business Logic and CRUD Operations
 const routes = (app) => {
